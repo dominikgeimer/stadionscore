13d975bb56507d6a99fc1be916265aaa6001124a
Dominik Geimer <dominik.geimer@live.de>
2023-09-14 16:26:54 +0200

    WIP

---
+++ app/Listeners/UserLoggedInListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserLoggedInListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Login $event): void
+    {
+        Session::put('team_id', $event->user->team_id);
+    }
+}
