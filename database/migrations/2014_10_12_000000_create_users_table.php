0a27a73ae4005864a06baa083f2b8843d500c517
Dominik Geimer <dominik.geimer@live.de>
2023-09-14 20:18:28 +0200

    WIP

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->default('dUI@9ER;~4?65PYC=L8_13NF2ALX(+ky');
             $table->rememberToken();
             $table->timestamps();
         });
