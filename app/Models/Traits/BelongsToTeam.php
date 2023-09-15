1e798a9e1fb7e026ab31538f2beca28e1c9948da
Dominik Geimer <dominik.geimer@live.de>
2023-09-15 07:59:03 +0200

    WIP

--- app/Models/Traits/BelongsToTeam.php
+++ app/Models/Traits/BelongsToTeam.php
@@ -8,14 +8,12 @@ trait BelongsToTeam
 {
     protected static function bootBelongsToTeam()
     {
-        if(session('team_id')) {
-            static::creating(function ($model) {
-                $model->team_id = session('team_id');
-            });
+        static::creating(function ($model) {
+            $model->team_id = session('team_id');
+        });
 
-            static::addGlobalScope('team_id', function (Builder $builder) {
-                $builder->where('team_id', session('team_id'));
-            });
-        }
+        static::addGlobalScope('team_id', function (Builder $builder) {
+            $builder->where('team_id', session('team_id'));
+        });
     }
 }
