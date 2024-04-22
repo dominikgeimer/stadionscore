9e2ee36ab6bc9f74f335094c3fc7d00a564b4fc0
Dominik Geimer <dominik.geimer@live.de>
2024-04-22 14:16:19 +0200

    WIP

--- database/migrations/2024_04_22_121206_create_clubs_table.php
+++ database/migrations/2024_04_22_121206_create_clubs_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('clubs', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('logo_url');
             $table->timestamps();
         });
     }
