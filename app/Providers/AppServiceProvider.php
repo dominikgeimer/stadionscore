c33aec6d0aacd0e0ce898e1a976971b983d1bc35
Dominik Geimer <dominik.geimer@live.de>
2024-04-22 14:12:16 +0200

    WIP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,7 +41,8 @@ class AppServiceProvider extends ServiceProvider
             'danger' => Color::Red,
             'gray' => Color::Zinc,
             'info' => Color::Blue,
-            'primary' => Color::Indigo,
+            'primary' => Color::Yellow,
+            //'primary' => Color::hex('#FDE100'),
             'success' => Color::Green,
             'warning' => Color::Amber,
         ]);
